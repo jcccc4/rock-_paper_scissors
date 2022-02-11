5a36e73f1ccec98db690df19d6b45b2c3974ff40
jcccc4 <jcsacra4@gmail.com>
2022-02-11 20:42:57 +0800

    added UI

--- scripts.js
+++ scripts.js
@@ -3,35 +3,74 @@ function computerPlay(selection){
     const comSelect= ['rock','paper','scissor']
     
     if(selection.toLowerCase() === 'rock' && comSelect[ranInt] === 'paper'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`)
+        return ['LOSE',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
     else if(selection.toLowerCase() === 'rock' && comSelect[ranInt] === 'scissor'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`
-    }
-    else if(selection.toLowerCase() === 'paper' && comSelect[ranInt] === 'rock'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`)
+        return ['WIN',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
     else if(selection.toLowerCase() === 'paper' && comSelect[ranInt] === 'scissor'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`)
+        return ['LOSE',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
-    else if(selection.toLowerCase() === 'scissor' && comSelect[ranInt] === 'paper'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`
+    else if(selection.toLowerCase() === 'paper' && comSelect[ranInt] === 'rock'){
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`)
+        return ['WIN',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
     else if(selection.toLowerCase() === 'scissor' && comSelect[ranInt] === 'rock'){
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU LOSE!!!`)
+        return ['LOSE',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
+    }
+    else if(selection.toLowerCase() === 'scissor' && comSelect[ranInt] === 'paper'){
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! YOU WON!!!`)
+        return ['WIN',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
     else{
-        return `${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! IT IS A TIE!!!`
+        console.log(`${selection.toUpperCase()} vs ${comSelect[ranInt].toUpperCase()}! IT IS A TIE!!!`)
+        return ['TIE',selection.toUpperCase(),comSelect[ranInt].toUpperCase()]
     }
     
 }
-console.log(computerPlay('rock'))
-console.log(computerPlay('paper'))
-console.log(computerPlay('scissor'))
-console.log(computerPlay('rock'))
-console.log(computerPlay('paper'))
-console.log(computerPlay('scissor'))
-console.log(computerPlay('rock'))
-console.log(computerPlay('paper'))
-console.log(computerPlay('scissor'))
 
+const man = document.querySelector(".manScore");
+const computer = document.querySelector('.compScore')
+const score = document.querySelector(".score-handler")
+const stat = document.querySelector(".status")
+var manWins = 0;
+var comWins = 0;
+
+const choice = document.querySelectorAll('.rps-container')
+
+
+function clicked(){
+    let play = computerPlay(this.id)
+    if( play[0] ==="WIN"){
+        stat.innerText = `MAN: ${play[1]}  \n COMPUTER: ${play[2]} \n YOU WIN!`
+        manWins++
+        man.innerText = `Man: ${manWins}`  
+    }
+    else if( play[0] === "LOSE"){
+        stat.innerText = `MAN: ${play[1]}  \n COMPUTER: ${play[2]} \n YOU LOSE!`
+        comWins++
+        computer.innerText = `Computer: ${comWins}` 
+    }else{
+        stat.innerText = `MAN: ${play[1]}  \n COMPUTER: ${play[2]} \n IT IS A TIE!`
+    }
+
+    if(manWins===5){
+        alert('YOU WIN')
+        manWins = 0;
+        comWins =0;
+        man.innerText = `Man: ${0}`
+        computer.innerText = `Man: ${0}`
+    }else if(comWins===5){
+        alert('YOU SUCK!')
+        manWins = 0;
+        comWins =0;
+        man.innerText = `Man: ${0}`
+        computer.innerText = `Man: ${0}`
+    }
+
+}
+choice.forEach(key=>key.addEventListener('click', clicked))
